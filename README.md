3c8fc1782c4ec62a4a898f190553c75d696cfbeb
Guillaume Gérard <guillaume.gerard88@gmail.com>
2018-04-06 11:20:30 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# gulp-demo
+
+This demo is building file from the `src` directory.
+It fingerprints every files and replace references in distribution css and generate a `rev-manifest.json` file accordingly.
+
+## Installation
+
+```sh
+npm install
+```
+
+## Build SCSS with fingerprints
+
+```sh
+gulp scss
+```
+
+## Build LESS with fingerprints
+
+```sh
+gulp less
+```
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const rev = require('gulp-rev');
+const override = require('gulp-rev-css-url');
+const sass = require('gulp-sass');
+const less = require('gulp-less');
+const merge = require('merge-stream');
+
+gulp.task('scss', () => {
+  let scss = gulp
+    .src('src/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('tmp'));
+  let images = gulp.src('src/*.jpg').pipe(gulp.dest('tmp'));
+  return merge(scss, images)
+    .pipe(rev())
+    .pipe(override())
+    .pipe(gulp.dest('dist'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('less', () => {
+  let scss = gulp
+    .src('src/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('tmp'));
+  let images = gulp.src('src/*.jpg').pipe(gulp.dest('tmp'));
+  return merge(scss, images)
+    .pipe(rev())
+    .pipe(override())
+    .pipe(gulp.dest('dist'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('dist'));
+});
